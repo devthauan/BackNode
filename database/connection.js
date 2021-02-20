40fab2cf6d0bb9e6889f74300318787dc74ec43d
devthauan <thauansilva05@hotmail.com>
2021-02-20 13:26:22 -0300

    brunosurfistinha

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,5 @@
 var knex = require('knex')({
     client: 'postgresql',
-    connection: {
-        host : 'ec2-54-144-251-233.compute-1.amazonaws.com',
-        user : 'pvkdysvgjmfrxu',
-        password : '5559e3bf7417029997fc421efb975ed18894c173f65b1392c46df0606100ac41',
-        database : 'dbimnfgl6mvsbq'
-     }
+    connection: process.env.DATABASE_URL
 });
 module.exports = knex
