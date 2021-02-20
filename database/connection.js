142a69fab16fd247f450e11654d90bf568064558
devthauan <thauansilva05@hotmail.com>
2021-02-20 12:30:23 -0300

    banco

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
-    client: 'mysql',
+    client: 'postgresql',
     connection: {
-        host : 'localhost',
-        user : 'root',
-        password : '',
-        database : 'vueBanco'
+        host : 'ec2-54-144-251-233.compute-1.amazonaws.com',
+        user : 'pvkdysvgjmfrxu',
+        password : '5559e3bf7417029997fc421efb975ed18894c173f65b1392c46df0606100ac41',
+        database : 'dbimnfgl6mvsbq'
      }
 });
 module.exports = knex
